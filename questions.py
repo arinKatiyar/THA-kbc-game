90bb6fbd0b9405b1f6a8d0f77257752f722f6e1f
Arin Katiyar <63258407+arinKatiyar@users.noreply.github.com>
2021-06-12 10:41:11 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,181 @@
+Questions = [
+
+    '''
+    Q1) The Trigger of gun is, colloquially, also known as what?
+   A.  Haathi
+   B.  Baagh
+   C.  Ghoda
+   D.  Oont
+   ''',
+    '''
+    Q2) Which of these numbers is normally required to verify a transaction, when shopping online using a debit or credit card?
+    A.	PAN
+    B.	PNR
+    C.	CVV
+    D.	AADHAR
+    ''',
+    '''
+    Q3) Which of these substances is composed of only two elements â€“ hydrogen & oxygen?
+    A.	Water
+    B.	Salt
+    C.	Sugar
+    D.	Blood
+    ''',
+    '''
+    Q4) Nabhah Sparsham Deeptam the motto of India Air Force, is taken from which ancient work?
+    A.  Garuda Purana
+    B.  Valmiki Ramayan
+    C.  Bhagwad Gita
+    D.  Mundaka Upnishad
+    ''',
+    '''    
+     Q5) In 2018, which Indian â€“ born person won the Field Medal, often called Noble Prize for Mathematics?
+    A.	Manjul Bhargava
+    B.	Akshay Venkatesh
+    C.	Vashishtha Narayan Singh
+    D.	S S Abhtankar
+    ''',
+    '''
+    Q6) Which of these stateâ€™s capital is farthest east?
+   A.  Arunachal Pradesh
+   B.  Nagaland
+   C.  Manipur
+   D.  Mizoram
+   ''',
+    '''
+    Q7) At which monument in Delhi did the British forces arrest Bahadur Shah Zafar in September 1857?
+    A.	Humayuâ€™s Tomb
+    B.	Nizamuddin Dargah
+    C.	Purana Qila
+    D.	Tughlaqabad Fort
+    ''',
+    '''
+    Q8) In the abbreviation PNR, used while issuing rail or air ticket, what does R stand For?
+    A.  Reservation
+    B.  Record
+    C.  Registration
+    D.  Recognition
+    ''',
+    '''
+    Q9) Which is the only one of these which comes in different sizes called mini, micro and nano?
+    A.	Trucks
+    B.	Credit Cards
+    C.	Cars
+    D.	SIM Cards
+    ''',
+    '''
+    Q10) Which of these creatures is not scientifically classified as fish?
+    A.  Rohu
+    B.  Katla
+    C.  Hilsa
+    D.  Jhinga
+    ''',
+    '''
+    Q11) Who among the following mythological figures got his name because of his ability to move his chariot in all directions?
+    A.	Adhiratha
+    B.	Dasharatha
+    C.	Jayadratha
+    D.	Partha
+    ''',
+    '''
+    Q12) Which is the largest joint in the human body?
+   A.  Elbow
+   B.  Hip
+   C.  Knee
+   D.  Shoulder
+   ''',
+    '''
+     Q13) Which Indian state had Indiaâ€™s first Muslim Woman Chief Minister?
+    A.  Kerala
+    B.  Jammu &Kashmir
+    C.  Goa
+    D.  Assam
+    ''',
+    '''
+     Q14) The only person to win two individual Nobel Prizes, not shared with anyone else, won them in what fields?
+    A.	Chemistry, Physics
+    B.	Chemistry, Peace
+    C.	Chemistry, Chemistry
+    D.	Literature, Peace
+    ''',
+    '''
+    Q15) In which of these sports are balls, ribbons, ropes, hoops and clubs used?
+   A.  Rhythmic Gymnastics
+   B.  Synchronized Swimming
+   C.  Rugby Sevens
+   D.  Beach Volleyball
+   ''',
+    '''
+    Q16) What kind of embroidery is this, which is believed to have been popularised by Mughal Empress Noor Jehan?
+   A.  Kantha
+   B.  Chikankari
+   C.  Gota Patti
+   D.  Phulkari
+   ''',
+    '''
+    Q17) Which of these creatures produces vermicompost?
+    A.	Cockroach
+    B.	Termite
+    C.	Earthworm
+    D.	Centipede
+    ''',
+    '''
+    Q18) Which of these athletes is known by the nickname â€˜Dhing Expressâ€™?
+    A.  Saikhom Mirabai Chanu
+    B.  Hima Das
+    C.  Manika Batra
+    D.  Manu Bhaker
+    ''',
+    '''
+    Q19) What does the â€˜Râ€™ stand for in the name of Dr B R Ambedkar?
+    A.  Ramesh
+    B.  Ramakant
+    C.  Raman
+    D.  Ramji
+    ''',
+    '''Q20) According to WHO, which disease has been completely eradicated from the world by vaccination?
+    A.  Measles
+    B.  Polio
+    C.  Mumps
+    D.  Smallpox
+    ''',
+    '''
+    Q21) Which of these measures is the shortest in length?
+    A.  Half Mile
+    B.  Half Foot
+    C.  Half Yard
+    D.  Half Metre
+    ''',
+    '''
+    Q22) Which of these is a board game which can normally be played by only two opponents at a time?
+   A.	Snakes & Ladders
+   B.	Chess
+   C.	Ludo
+   D.	Monopoly
+   ''',
+    '''
+    Q23) Common cold, polio and AIDS are diseases caused by which of these organisms?
+    A.  Bacteria
+    B.  Virus
+    C.  Protozoa
+    D.  Fungus
+    ''',
+    '''
+    Q24) In which century were the first two battles of Panipat fought?
+    A.  15th
+    B.  16th
+    C.  18th
+    D.  17th
+    ''',
+    '''
+    Q25) The twenty seven daughters of Daksha collectively known as Nakshatras, are the Wives of which God?
+    A.  Surya
+    B.  Chandra
+    C.  Mangal
+    D.  Budha
+    ''',
+
+]
+
+Answers = ['C', 'C', 'A', 'C', 'B', 'B', 'A', 'B', 'D', 'D', 'B', 'C', 'D', 'B', 'A', 'B', 'C', 'B', 'D', 'D',
+           'B', 'B', 'B', 'B', 'B', 'D']
